2d8316e333e5c4a31c0826972da281dae5283031
Наталья Иванова <natashaneti96@yandex.ru>
2022-04-17 10:28:46 +0300

    текст

---
+++ yatube/posts/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+# Из модуля models импортируем модель Post
+from .models import Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',) 
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',) 
+    list_editable = ('group',)
+    empty_value_display = '-пусто-'
+    
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Post, PostAdmin) 
+# Register your models here.
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    search_fields = ('description',)
+
+admin.site.register(Group, GroupAdmin)
